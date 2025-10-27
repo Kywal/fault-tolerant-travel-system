18ca4ac0240efa2e620d2f271da9322b543c0db4
devHebertfst <hebertfst@hotmail.com>
2025-10-26 21:41:02 -0300

    refact: change request

--- fidelity/src/main/java/br/ufrn/imd/fidelity/FidelityController.java
+++ fidelity/src/main/java/br/ufrn/imd/fidelity/FidelityController.java
@@ -2,6 +2,8 @@ package br.ufrn.imd.fidelity;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FidelityController {
 
-  @GetMapping("/bonus")
-  public ResponseEntity<Void> registerBonus(@RequestParam("user") String userId, @RequestParam("bonus") int bonusValue) {
-      System.out.println(
-          "[Fidelity] Bônus registrado para usuário: " + userId +
-          " | Valor do bônus: " + bonusValue
-      );
-      return ResponseEntity.ok().build();
-  }
+  @PostMapping("/bonus")
+public ResponseEntity<Void> registerBonus(@RequestBody BonusRequest request) {
+    System.out.println(
+        "[Fidelity] Bônus registrado para usuário: " + request.getUser() +
+        " | Valor do bônus: " + request.getBonus()
+    );
+    return ResponseEntity.ok().build();
+}
+
+  public static class BonusRequest {
+        private String user;
+        private int bonus;
+        
+        public String getUser() { return user; }
+        public void setUser(String user) { this.user = user; }
+        public int getBonus() { return bonus; }
+        public void setBonus(int bonus) { this.bonus = bonus; }
+    }
 }
