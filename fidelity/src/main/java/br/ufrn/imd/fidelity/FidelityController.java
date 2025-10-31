5cdbdafb07bd5b461957a17f88e640678e5f58b9
Emanuel Kywal <emanuelkywal@outlook.com>
2025-10-31 02:29:03 -0300

    refact: use log instead of Sysout and turn DTO into java record on Fidelity

--- fidelity/src/main/java/br/ufrn/imd/fidelity/FidelityController.java
+++ fidelity/src/main/java/br/ufrn/imd/fidelity/FidelityController.java
@@ -1,32 +1,21 @@
 package br.ufrn.imd.fidelity;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@Slf4j
 @RestController
 public class FidelityController {
 
-  @PostMapping("/bonus")
-public ResponseEntity<Void> registerBonus(@RequestBody BonusRequest request) {
-    System.out.println(
-        "[Fidelity] Bônus registrado para usuário: " + request.getUser() +
-        " | Valor do bônus: " + request.getBonus()
-    );
-    return ResponseEntity.ok().build();
-}
-
-  public static class BonusRequest {
-        private String user;
-        private int bonus;
-        
-        public String getUser() { return user; }
-        public void setUser(String user) { this.user = user; }
-        public int getBonus() { return bonus; }
-        public void setBonus(int bonus) { this.bonus = bonus; }
+    @PostMapping("/bonus")
+    public ResponseEntity<Void> registerBonus(@RequestBody BonusRequest request) {
+        log.info("[Fidelity] Bônus registrado para usuário: {} | Valor do bônus: {}", request.user(), request.bonus());
+        return ResponseEntity.ok().build();
     }
+
+    public record BonusRequest(String user, int bonus) { }
+
 }
